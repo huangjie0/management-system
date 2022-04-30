65416257a4c451a173dabd5e17fd40ddbbab1855
huangjie <1364045672@qq.com>
2022-04-30 11:21:03 +0800

    优化

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
-      <el-button>按钮</el-button>
-      <el-button type="primary">主要按钮</el-button>
+    <el-button>按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-radio-group v-model="radio">
+      <el-radio :label="3">备选项</el-radio>
+      <el-radio :label="6">备选项</el-radio>
+      <el-radio :label="9">备选项</el-radio>
+    </el-radio-group>
   </div>
 </template>
 
 <script>
 export default {
-    name:'HelloWorld'
-}
+  name: "HelloWorld",
+};
 </script>
 
 <style>
-
 </style>
