006e496740faa18a477bee48d4b7e267da9edcb2
huangjie <1364045672@qq.com>
2022-04-30 21:23:44 +0800

    优化

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,3 +2,16 @@ import axios from "axios";
 import config from './config/index'
 //判断环境变量
 const baseURL = process.env.NODE_ENV==='dev' ? config.baseURL.dev:config.baseURL.pro;
+class HttpRequest {
+    constructor (baseURL){
+        this.baseURL = baseURL
+    }
+    getInsideConfig(){
+        const config={
+            baseURL:this.baseURL,
+            header:{}
+        }
+        return config
+    }
+}
+export default new HttpRequest(baseURL);
