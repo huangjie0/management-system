c9d1c70284e629489faa0c9a8a6859754c5a6c22
huangjie <1364045672@qq.com>
2022-04-30 11:11:22 +0800

    优化

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
+    <HelloWorld/>
   </div>
 </template>
 
 <script>
-
+import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
+    HelloWorld
   }
 }
 </script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <el-button>按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'HelloWorld'
+}
+</script>
+
+<style>
+
+</style>
