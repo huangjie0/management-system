e0681b3116dd0c064f6d5302dc11154e423ca884
huangjie <1364045672@qq.com>
2022-05-01 17:13:56 +0800

    优化

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -26,119 +26,184 @@
         </el-table>
       </el-card>
     </el-col>
-    <el-col :span="16" style="margin-top:20px;">
-        <div class="num">
-            <el-card v-for="item in countDate" :key="item.name" :body-style="{display:'flex',padding:0}">
-                <i class="icon" :class="`el-icon-${item.icon}`" :style="{background:item.color}"></i>
-                <div class="detail">
-                    <p class="num">￥{{item.value}}</p>
-                    <p class="txt">{{item.name}}</p>
-                </div>
-            </el-card>
-        </div>
-        <el-card style="height:280px">
-          <div style="height:280px" ref="echarts"></div>
+    <el-col :span="16" style="margin-top: 20px">
+      <div class="num">
+        <el-card
+          v-for="item in countDate"
+          :key="item.name"
+          :body-style="{ display: 'flex', padding: 0 }"
+        >
+          <i
+            class="icon"
+            :class="`el-icon-${item.icon}`"
+            :style="{ background: item.color }"
+          ></i>
+          <div class="detail">
+            <p class="num">￥{{ item.value }}</p>
+            <p class="txt">{{ item.name }}</p>
+          </div>
         </el-card>
-        <div class="graph">
-            <el-card style="height:260px"></el-card>
-            <el-card style="height:260px"></el-card>
-        </div>
+      </div>
+      <el-card style="height: 280px">
+        <div style="height: 280px" ref="echarts"></div>
+      </el-card>
+      <div class="graph">
+        <el-card style="height: 260px">
+          <div style="height:240px" ref="userEcharts"> 
+
+          </div>
+        </el-card>
+        <el-card style="height: 260px"></el-card>
+      </div>
     </el-col>
   </el-row>
 </template>
 
 <script>
-import {getData} from '../../api/data'
-import * as echarts from 'echarts'
+import { getData } from "../../api/data";
+import * as echarts from "echarts";
 export default {
   name: "Home",
   data() {
-      return {
+    return {
       userImg: require("../../assets/images/user.png"),
       tableData: [],
       tableLabel: {
-          name: "课程",
-          todayBuy: "今日购买",
-          monthBuy: "本月购买",
-          totalBuy: "总购买",
+        name: "课程",
+        todayBuy: "今日购买",
+        monthBuy: "本月购买",
+        totalBuy: "总购买",
       },
       //统计数据
       countDate: [
-          {
+        {
           name: "今日支付订单",
           value: 1234,
           icon: "success",
           color: "#2ec7c9",
-          },
-          {
+        },
+        {
           name: "今日收藏订单",
           value: 210,
           icon: "star-on",
           color: "#ffb980",
-          },
-          {
+        },
+        {
           name: "今日未支付订单",
           value: 1234,
           icon: "s-goods",
           color: "#5ab1ef",
-          },
-          {
+        },
+        {
           name: "本月支付订单",
           value: 1234,
           icon: "success",
           color: "#2ec7c9",
-          },
-          {
+        },
+        {
           name: "本月收藏订单",
           value: 210,
           icon: "star-on",
           color: "#ffb980",
-          },
-          {
+        },
+        {
           name: "本月未支付订单",
           value: 1234,
           icon: "s-goods",
           color: "#5ab1ef",
-          },
+        },
       ],
     };
   },
-   mounted() {
-    getData().then(res=>{
-      const {code,data} = res.data
-      if(code==20000){
-        this.tableData = data.tableData
-        const order = data.orderData
-        const xData = order.date
-        const keyArray = Object.keys(order.data[0])
-        const series = []
-        keyArray.forEach(key=>{
+  mounted() {
+    getData().then((res) => {
+      const { code, data } = res.data;
+      if (code == 20000) {
+        this.tableData = data.tableData;
+        const order = data.orderData;
+        const xData = order.date;
+        const keyArray = Object.keys(order.data[0]);
+        const series = [];
+        keyArray.forEach((key) => {
           series.push({
-            name:key,
-            data:order.data.map(item=>
-              item[key] 
-            ),
-            type:'line'
-          })
-        })
-        const option={
-          xAxis:{
-            data:xData
+            name: key,
+            data: order.data.map((item) => item[key]),
+            type: "line",
+          });
+        });
+        const option = {
+          xAxis: {
+            data: xData,
           },
-          yAxis:{},
-          legend:{
-            data:keyArray
+          yAxis: {},
+          legend: {
+            data: keyArray,
           },
-          series
-        }
+          series,
+        };
         const E = echarts.init(this.$refs.echarts);
         E.setOption(option);
+        //用户图
+        const userOption = {
+          legend: {
+            // 图例文字颜色
+            textStyle: {
+              color: "#333",
+            },
+          },
+          grid: {
+            left: "20%",
+          },
+          // 提示框
+          tooltip: {
+            trigger: "axis",
+          },
+          xAxis: {
+            type: "category", // 类目轴
+            data: data.userData.map(item=>
+              item.date
+              ),
+            axisLine: {
+              lineStyle: {
+                color: "#17b3a3",
+              },
+            },
+            axisLabel: {
+              interval: 0,
+              color: "#333",
+            },
+          },
+          yAxis: [
+            {
+              type: "value",
+              axisLine: {
+                lineStyle: {
+                  color: "#17b3a3",
+                },
+              },
+            },
+          ],
+          color: ["#2ec7c9", "#b6a2de"],
+          series: [
+            {
+              name:'新增用户',
+              data:data.userData.map(item=>item.new),
+              type:'bar'
+            },
+            {
+              name:'活跃用户',
+              data:data.userData.map(item=>item.active),
+              type:'bar'
+            }
+          ],
+        };
+        const U = echarts.init(this.$refs.userEcharts);
+        U.setOption(userOption);
       }
-    })
+    });
   },
 };
 </script>
 
 <style>
-
 </style>
